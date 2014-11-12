9592db60ab87fb83b3881a470a5b4ec19f807123
dmxiaoshen <dmxiaoshen@163.com>
2014-11-12 11:30:23 +0800

    add HelloWorld.java and modify HelloGit.java

--- src/com/dmxiaoshen/git/HelloGit.java
+++ src/com/dmxiaoshen/git/HelloGit.java
@@ -5,4 +5,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
     }
+    
+    public static void display(){
+        System.out.println("Hello eclipse");
+    }
 }
